c2187488e25edbdfaa806d9e6f5826d34e749909
Namo.Dev <pitsanunvc97@gmail.com>
2026-02-11 07:41:29 +0000

    gg

--- Node.h
+++ Node.h
@@ -20,16 +20,31 @@ typedef struct node* NodePtr;
 void enqueue(NodePtr * head, NodePtr* tail, int x){
   Node* new_node=(NodePtr) malloc(sizeof(Node));
 if(new_node){ 
+  new_node->data=x;
+  new_node->nextPtr=NULL;
     /* Finish queue*/
+  if((*head)==NULL){//หัวแถว
+    *head=new_node;
+  }
+  else{//ต่อแถว
+    (*tail)->nextPtr=new_node;
+  }
+  *tail = new_node;
  }
 }
 
 
 int dequeue(NodePtr* head, NodePtr* tail){
   NodePtr t=*head;
-   if(t){
+  if(t){
    int value= t->data;
    /* Finish dequeue*/
+   *head=t->nextPtr;
+   if(*head==NULL) *tail=NULL;
+   free(t);
+   //Normal case
+
+   //Last node
        
        
    return value;
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-argument: main.c 
+compile: main.c 
 	gcc main.c -o Q
 run: Q
-	./Q
+	./Q 21 y 454 x 21 02 x 3 x x
